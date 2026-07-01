cd84d76cdfd7b501f0c7dfe745ecd73fbb95b1d5
Shivani <shivanigolem5@gmail.com>
2026-07-01 20:01:14 +0530

    updated day-1.py

--- day-1.py
+++ day-1.py
@@ -4,6 +4,7 @@
 # Print numbers from 1 to 10, but skip 5 using continue.
 # Print numbers from 1 to 10, but stop when the number becomes 8 using break.
 
+#1.
 a=2
 if a>0:
     print("positive")
@@ -14,12 +15,34 @@ else:
 
 print("-------------------------")
 
+#2.
 for i in range(1,11):
     print("7*", i, " = ",i*7)
 
 print("--------------------------")
 
+#3.
 a=10
 while a>=1:
     print(a)
     a-=1
+
+print("---------------------------")
+
+#4.
+for i in range(1, 11):
+
+    if i == 5:
+        continue
+
+    print(i)
+
+print("---------------------------")
+
+#5.
+for i in range(1, 11):
+
+    if i == 8:
+        break
+
+    print(i)
